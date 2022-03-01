8e14194514aebf3b662f2b23d9d1fbb915625947
Sayan <sayan.sayanak2000@gmail.com>
2022-02-28 22:49:55 -0700

    Board value modified

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,19 +5,19 @@ const Board = () => {
   return (
     <div className="board">
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </div>
   );
